c1323000667d0c34eae7e18a3e5fde4e037ad598
mshherbakov <sherbakov.maks@gmail.com>
2020-03-14 01:00:30 +0500

    initial commit

---
+++ src/main/java/ru/sherb/tinkrep/Launch.java
@@ -0,0 +1,12 @@
+package ru.sherb.tinkrep;
+
+/**
+ * @author maksim
+ * @since 14.03.2020
+ */
+public class Launch {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
